fa4c39b3b6ac22c09c49e5abed18d302efcd7452
Haikal Abdurrahman <74368814+ABDhaikal@users.noreply.github.com>
2025-04-12 21:17:53 +0700

    Register (#3)
    belum pull req register, cuma edit tabel sama dependency
    
    ---------
    
    Co-authored-by: ABDhaikal <kakikuatku.gmail.com>

---
+++ src/lib/jwt.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { TokenExpiredError, verify } from "jsonwebtoken";
+import { JWT_SECRET } from "../config/env";
+import { ApiError } from "../utils/api-error";
+export const verifyToken = async (
+   req: Request,
+   res: Response,
+   next: NextFunction
+) => {
+   const token = req.headers.authorization?.split(" ")[1];
+
+   if (!token) {
+      throw new ApiError("Token is missing", 401);
+   }
+
+   verify(token, JWT_SECRET as string, (err, payload) => {
+      if (err) {
+         if (err instanceof TokenExpiredError) {
+            throw new ApiError("Token expired", 401);
+         } else {
+            throw new ApiError("Invalid token", 401);
+         }
+      }
+      res.locals.user = payload;
+
+      next();
+   });
+};
