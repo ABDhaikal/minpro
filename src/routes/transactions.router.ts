cd12c38d936ee26d56561ede4e5e28385c38a447
minaimnida <imaduddinmuhammad92@gmail.com>
2025-04-18 15:05:09 +0700

    feat: enhance Transaction model and update related services for payment proof handling

--- src/routes/transactions.router.ts
+++ src/routes/transactions.router.ts
@@ -1,29 +1,29 @@
-import { Router } from "express";
-import { createTransactionController, uploadPaymentProofController } from "../controllers/transactions.controller";
-import { verifyToken } from "../lib/jwt";
-import { verifyRole } from "../middlewares/role.middleware";
-import { validateCreateTransaction } from "../validators/transaction.validator";
-import multer from "multer"
+// import { Router } from "express";
+// import { createTransactionController, uploadPaymentProofController } from "../controllers/transactions.controller";
+// import { verifyToken } from "../lib/jwt";
+// import { verifyRole } from "../middlewares/role.middleware";
+// import { validateCreateTransaction } from "../validators/transaction.validator";
+// import multer from "multer"
 
 
-const router = Router();
+// const router = Router();
 
-const upload = multer({storage: multer.memoryStorage()})
+// const upload = multer({storage: multer.memoryStorage()})
 
-router.post(
-  "/",
-  verifyToken,
-  verifyRole(["USER"]),
-  validateCreateTransaction,
-  createTransactionController
-);
+// router.post(
+//   "/",
+//   verifyToken,
+//   verifyRole(["USER"]),
+//   validateCreateTransaction,
+//   createTransactionController
+// );
 
-router.post(
-    "/transactions/:transactioId/payment-proof",
-    verifyToken,
-    verifyRole(["USER"]),
-    upload.single("imageTransaction"),
-    uploadPaymentProofController
-  );
+// router.post(
+//     "/transactions/:transactioId/payment-proof",
+//     verifyToken,
+//     verifyRole(["USER"]),
+//     upload.single("imageTransaction"),
+//     uploadPaymentProofController
+//   );
 
-export default router;
+// export default router;
--- src/services/transactions/create-transaction.service.ts
+++ src/services/transactions/create-transaction.service.ts
@@ -1,66 +1,102 @@
-import { Transaction, TransactionStatus } from "@prisma/client";
-import prisma from "../../config/prisma";
-import { ApiError } from "../../utils/api-error";
-import { Backoffs } from "bullmq";
-import { userTransactionQueue } from "../../jobs/queue/transaction.queue";
+// import { Ticket, Transaction, TransactionStatus } from "@prisma/client";
+// import prisma from "../../config/prisma";
+// import { ApiError } from "../../utils/api-error";
+// import { Backoffs } from "bullmq";
+// import { userTransactionQueue } from "../../jobs/queue/transaction.queue";
 
-export const createTransactionService = async (
-  body: Pick<Transaction, "pointsUsed" | "totalPrice" | "paymentDeadline">,
-  authUserId: string
-) => {
-    
-  if (!body.pointsUsed || !body.totalPrice || !body.paymentDeadline) {
-    throw new ApiError("Invalid input data", 400);
-  }
+// interface Iticket {
+//   ticketId: string;
+//   amount: number;
+// }
 
-  const user = await prisma.user.findUnique({
-    where: { id: authUserId },
-  });
+// interface ICreateTransactionService {
+//   cuponID: string[];
+//   tickets: Iticket[];
+//   pointsUsed: number;
+// }
+// export const createTransactionService = async (
+//   body: ICreateTransactionService,
+//   authUserId: string
+// ) => {
+//   // ngecek user yang mau transaksi bener apa engga (ngecek auth)
 
-  if (!user) {
-    throw new ApiError("User not found", 404);
-  }
+//   // ngecek cuponya ada apa engga
+//   // ngecek kuponya punya user yang login bukan
+//   // ngecek pointused mencukupi apa engga
 
-  const newTransaction = await prisma.$transaction(async (tx) => {
-    //kalo user mau pake point??
-    if (body.pointsUsed > 0) {
-      const updatedUser = await tx.user.update({
-        where: { id: authUserId },
-        data: { point: { decrement: body.pointsUsed } },
-      });
+//   // ngecek tiketnya ada apa engga
+//   // ngecek daftar tiketnya dari event yang sama engga
+//   // ngecek daftar tiketnya tersedia apa engga
+//   // jumlah semua harga dikali jumlah ticket yang mau dibeli
 
-      if (updatedUser.point < 0) {
-        throw new ApiError("Insufficient points", 400);
-      }
-    }
+//   // njumlah diskon dari kupon
+//   //bikin total harga yang sudah dikurangi diskon
+//   // menentukan deadline payment proof
+//   // nambah data buyed di semua tiket yang mau dibeli
 
-    return await tx.transaction.create({
-      data: {
-        userId: authUserId,
-        status: TransactionStatus.WAITING_FOR_ADMIN_CONFIRMATION, //tidak ada PENDING??
-        pointsUsed: body.pointsUsed,
-        totalPrice: body.totalPrice,
-        paymentDeadline: body.paymentDeadline,
-      },
-    });
-  });
+//   // bikin table transaski
+//   // bikin table transaction ticket dari tickets
+//   // bikin table cupon transaction
 
-  await userTransactionQueue.add(
-    "new-transaction",
-    {
-      uuid: newTransaction.id,
-    },
-    {
-      jobId: newTransaction.id,
-      delay: 2 * 60000,
-      removeOnComplete: true,
-      attempts: 5,
-      backoff: {
-        type: "exponential",
-        delay: 1000,
-      },
-    }
-  );
+//   // bikin antrian redis
 
-  return { messaage: "Transaction created successfully", data: newTransaction };
-};
+//   const user = await prisma.user.findUnique({
+//     where: { id: authUserId },
+//   });
+
+//   if (body.cuponID) {
+//     const cupon = await prisma.cuponDiscount.findMany({
+//       where: { id: authUserId },
+//     });
+//     if (!body.cuponID) {
+//       throw new ApiError("Cupon not found", 404);
+//     }
+//   }
+
+//   if (!user) {
+//     throw new ApiError("User not found", 404);
+//   }
+
+//   const newTransaction = await prisma.$transaction(async (tx) => {
+//     //kalo user mau pake point??
+//     if (body.pointsUsed > 0) {
+//       const updatedUser = await tx.user.update({
+//         where: { id: authUserId },
+//         data: { point: { decrement: body.pointsUsed } },
+//       });
+
+//       if (updatedUser.point < 0) {
+//         throw new ApiError("Insufficient points", 400);
+//       }
+//     }
+
+//     return await tx.transaction.create({
+//       data: {
+//         userId: authUserId,
+//         status: TransactionStatus.WAITING_FOR_ADMIN_CONFIRMATION, //tidak ada PENDING??
+//         pointsUsed: body.pointsUsed,
+//         totalPrice: body.totalPrice,
+//         paymentDeadline: body.paymentDeadline,
+//       },
+//     });
+//   });
+
+//   await userTransactionQueue.add(
+//     "new-transaction",
+//     {
+//       uuid: newTransaction.id,
+//     },
+//     {
+//       jobId: newTransaction.id,
+//       delay: 2 * 60000,
+//       removeOnComplete: true,
+//       attempts: 5,
+//       backoff: {
+//         type: "exponential",
+//         delay: 1000,
+//       },
+//     }
+//   );
+
+//   return { messaage: "Transaction created successfully", data: newTransaction };
+// };
